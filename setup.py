e597fba6b7c4151eeed9c17b896890d02b21a51b
DePacifier <milkybekele@gmail.com>
2021-08-25 10:16:10 +0300

    set requirements and setup files for the project

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirement_file:
+    requirements_list = requirement_file.readlines()
+    requirements_list = [lib.replace('\n', '') for lib in requirements_list]
+
+requirements = requirements_list
+
+test_requirements = []
+
+setup(
+    author="Same Michael, Milky Bekele, Eyerusalem",
+    author_email="milkybekele@gmail.com",
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="Constructing a Casual Graph for Breast Cancer Disease.",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/CasualModel/CancerCausality',
+    version='0.1.0',
+    zip_safe=False,
+)
